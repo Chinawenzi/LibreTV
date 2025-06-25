665b3c8fdf3900fee48b2cc92e199f01b453ba7b
一只大蚊子 <95013919+Chinawenzi@users.noreply.github.com>
2025-06-25 17:13:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -47,12 +47,7 @@ const API_SITES = {
         api: 'http://ffzy5.tv/api.php/provide/vod',
         name: '非凡影视',
         detail: 'http://ffzy5.tv', 
-    },
-    heimuer: {
-        api: 'https://json.heimuer.xyz/api.php/provide/vod',
-        name: '黑木耳',
-        detail: 'https://heimuer.tv', 
-    },
+     },
     zy360: {
         api: 'https://360zy.com/api.php/provide/vod',
         name: '360资源',
